ed0fad930186f5154467acc828bd881e86938a42
Ilya Dvorkin <ilya.dvorkin@ownera.io>
2024-07-14 20:30:06 +0300

    added hash sort

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,49 @@
-use yaml_rust2::{YamlLoader};
+use yaml_rust2::{YamlEmitter, YamlLoader};
 use yaml_rust2::yaml::{Array, Yaml};
 
-fn walk(doc: &mut Yaml, key: &str) {
+fn walk(doc: &mut Yaml, key: &str, pre_order_yaml: &str) {
     match doc {
         Yaml::Array(ref mut v) => {
-            replacer(v, key);
+            array_sorter(v, key);
             for x in v {
-                walk(x, key);
+                walk(x, key, pre_order_yaml);
             }
         }
         Yaml::Hash(ref mut h) => {
+            hash_sorter(h, pre_order_yaml);
             for (_, v) in h {
-                walk(v, key);
+                walk(v, key, pre_order_yaml);
             }
         }
         _ => {}
     }
 }
 
-fn replacer(array: &mut Array, key: &str) {
+fn hash_sorter(hash: &mut yaml_rust2::yaml::Hash, pre_order_yaml: &str) {
+    let pre_order_docs = YamlLoader::load_from_str(pre_order_yaml).unwrap();
+    let pre_order_doc = &pre_order_docs[0];
+    let pre_order_array = pre_order_doc.as_vec().unwrap();
+    let mut result = yaml_rust2::yaml::Hash::new();
+
+    // Sort the hash by the pre_order_array
+    for key in pre_order_array {
+        if hash.contains_key(key) {
+            result.insert(key.clone(), hash.remove(key).unwrap());
+        }
+    }
+
+    // Sort the rest of the hash
+    let mut entries: Vec<_> = hash.into_iter().collect();
+    entries.sort_by_key(|entry| entry.0);
+    for (key, value) in entries {
+        result.insert(key.clone(), value.clone());
+    }
+
+    *hash = result;
+
+}
+
+fn array_sorter(array: &mut Array, key: &str) {
     array.sort_by(|a, b| {
         let a_str = a[key].as_str();
         let b_str = b[key].as_str();
@@ -32,32 +57,91 @@ fn replacer(array: &mut Array, key: &str) {
 }
 
 fn main() {
+    let pre_order = r#"
+      - enabled
+      - apiVersion
+      - kind
+      - metadata
+      - name
+      - namespace
+      - labels
+      - annotations
+      - goTemplate
+      - generators
+      - spec
+      - containers
+      - image
+      - ports
+      - protocol
+      - resources
+      - limits
+      - requests
+      - cpu
+      - memory
+      - volumeMount
+      - destination
+      - sources
+    "#;
+
     let s = r#"
-      arr:
-      - name: c
-        value: 3
-      - name: b
-        value: 2
-      - name: a
-        value: 1
+        test: yaml
+        namespace: argocd
+        name: test
+        apiVersion: argoproj.io/v1alpha1
         arr:
-        - name: z
-          value: 99
-        - name: y
-          value: 88
-        - name: x
-          value: 77
-      arr2:
-        - zaza
+          - test: without sort key
+          - test: yaml
+            namespace: argocd
+            name: test
+            apiVersion: argoproj.io/v1alpha1
+            arr2:
+              - namespace: argocd
+                name: test
+                apiVersion: argoproj.io/v1alpha1
+                test: yaml
+                enabled: false
+          - enabled: false
+            name: arr
+        enabled: false
+        spec:
+          generators:
+            - list:
+                elements:
+                  - name: c
+                  - name: b
+                  - name: a
+                    ord:
+                      - name: c
+                      - name: b
+                      - name: a
+
+          name: cluster-resources
+
+        # Comment
+        anchor: &test
+          - anchor
+        anchor-test: *test
     "#;
 
     let mut docs = YamlLoader::load_from_str(s).unwrap();
     let doc = &mut docs[0];
 
-    println!("Before: {:?}", doc);
+    // Dump the YAML object
+    let mut out_str = String::new();
+    {
+        let mut emitter = YamlEmitter::new(&mut out_str);
+        emitter.dump(doc).unwrap();
+    }
+    println!("Before:\n{}\n\n", out_str);
 
-    walk(doc, "name");
+    walk(doc, "name", pre_order);
 
-    println!("After: {:?}", doc);
+    // Dump the YAML object
+    let mut out_str = String::new();
+    {
+        let mut emitter = YamlEmitter::new(&mut out_str);
+        emitter.dump(doc).unwrap();
+    }
+    println!("After:\n{}", out_str);
 
 }
