de50730c19d7a9e2b0b20f37864e29c3ec0d5e50
Ilya Dvorkin <ilya.dvorkin@ownera.io>
2024-07-17 14:31:40 +0300

    added full test

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,6 +1,6 @@
 use yaml_sorter_rust::config::{Config, set_config};
 use yaml_sorter_rust::processors::{process_yaml};
-use yaml_rust2::{YamlLoader, Yaml};
+use yaml_rust2::{YamlLoader, Yaml, YamlEmitter};
 
 pub fn init_test_config(mock_config: &str) -> Config {
     let config_yaml = YamlLoader::load_from_str(mock_config).unwrap();
@@ -90,4 +90,126 @@ fn test_array_sorter() {
             ),
         ]
     ));
+}
+
+#[test]
+fn full_test() {
+    let config = init_test_config(
+        "
+        sortKey: name
+        preOrder:
+          - enabled
+          - apiVersion
+          - kind
+          - metadata
+          - name
+          - namespace
+          - labels
+          - annotations
+          - goTemplate
+          - generators
+          - spec
+          - containers
+          - image
+          - ports
+          - protocol
+          - resources
+          - limits
+          - requests
+          - cpu
+          - memory
+          - volumeMount
+          - destination
+          - sources
+        "
+    );
+
+    let test_str = r#"
+test: yaml
+namespace: argocd
+name: test
+apiVersion: argoproj.io/v1alpha1
+arr:
+  - test: without sort key
+  - test: yaml
+    namespace: argocd
+    name: test
+    apiVersion: argoproj.io/v1alpha1
+    arr2:
+      - namespace: argocd
+        name: test
+        apiVersion: argoproj.io/v1alpha1
+        test: yaml
+        enabled: false
+  - enabled: false
+    name: arr
+enabled: false
+spec:
+  generators:
+    - list:
+        elements:
+          - name: c
+          - name: b
+          - name: a
+            ord:
+              - name: c
+              - name: b
+              - name: a
+
+  name: cluster-resources
+
+# Comment
+anchor: &test
+  - anchor
+anchor-test: *test
+    "#;
+
+    let result = r#"---
+enabled: false
+apiVersion: argoproj.io/v1alpha1
+name: test
+namespace: argocd
+spec:
+  name: cluster-resources
+  generators:
+    - list:
+        elements:
+          - name: a
+            ord:
+              - name: a
+              - name: b
+              - name: c
+          - name: b
+          - name: c
+anchor:
+  - anchor
+anchor-test:
+  - anchor
+arr:
+  - enabled: false
+    name: arr
+  - apiVersion: argoproj.io/v1alpha1
+    name: test
+    namespace: argocd
+    arr2:
+      - enabled: false
+        apiVersion: argoproj.io/v1alpha1
+        name: test
+        namespace: argocd
+        test: yaml
+    test: yaml
+  - test: without sort key
+test: yaml"#;
+
+    let mut docs = YamlLoader::load_from_str(test_str).unwrap();
+    let doc = &mut docs[0];
+    process_yaml(doc, &config);
+
+    let mut out_str = String::new();
+    {
+        let mut emitter = YamlEmitter::new(&mut out_str);
+        emitter.dump(doc).unwrap();
+    }
+
+    assert_eq!(out_str, result);
 }
